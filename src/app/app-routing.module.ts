80fe9392118f80ae3e2f9bc00fd62a1c597634ad
Seraphim <kuderskiy.denis@gmail.com>
2017-12-23 20:30:57 +0200

    Changes to make AoT compilation available to deploy application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from "@angular/router";
+import { Routes, RouterModule, PreloadAllModules } from "@angular/router";
 import { HomeComponent } from './core/home/home.component';
 import { ErrorPageComponent } from './core/error-page/error-page.component';
 
@@ -14,7 +14,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})
   ],
   exports: [
     RouterModule
